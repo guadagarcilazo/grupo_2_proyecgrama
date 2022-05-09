bad389117ca3f807b839c82d1ca50e0ac1fc2129
Natali <natali.lescano@arbusta.net>
2022-05-09 17:50:53 -0300

    cambios

--- js/presentacion-css.js
+++ js/presentacion-css.js
@@ -9,3 +9,40 @@ window.addEventListener('scroll', () => {
         header.style.removeProperty('background');
     }
 });
+
+/* Preguntas desplegables */
+
+const items = document.querySelectorAll('.preg__item');
+const estados = document.querySelectorAll('.estado');
+
+// recorro el array para darle añadirle un evento a cada elemento
+items.forEach( e => {    
+
+    const icon = e.firstElementChild.lastElementChild.firstElementChild;
+
+    // acá le añado el evento a cada elemento del array
+    e.addEventListener('click', () =>{
+
+        if(!e.classList.contains('open')){
+
+            // cambio clases a los elementos
+            e.classList.add('open');
+            e.classList.remove('closed');
+            e.lastElementChild.classList.remove('opciones-cerrado');           
+            e.lastElementChild.classList.add('opciones-abierto');
+            icon.classList.remove('rotate0');
+            icon.classList.add('rotate90');
+        
+        } else {
+            
+            // cambio clases a los elementos
+            e.classList.add('closed');
+            e.classList.remove('open');
+            e.lastElementChild.classList.remove('opciones-abierto');    
+            e.lastElementChild.classList.add('opciones-cerrado');
+            icon.classList.remove('rotate90');
+            icon.classList.add('rotate0');
+        }
+
+    });
+});
